b494da0bbf18c2e6492be17f5dd4ac9b09f6c121
vekatze <vekatze@icloud.com>
2024-09-01 15:09:37 +0900

    initial sketch

---
+++ source/foreign.c
@@ -0,0 +1,61 @@
+#include <curl/curl.h>
+
+__attribute__((always_inline))
+int neut_rin_v0_1_CURL_GLOBAL_DEFAULT() {
+  return CURL_GLOBAL_DEFAULT;
+}
+
+__attribute__((always_inline))
+int neut_rin_v0_1_CURLINFO_RESPONSE_CODE() {
+  return CURLINFO_RESPONSE_CODE;
+}
+
+__attribute__((always_inline))
+int neut_rin_v0_1_CURLOPT_CUSTOMREQUEST() {
+  return CURLOPT_CUSTOMREQUEST;
+}
+
+__attribute__((always_inline))
+int neut_rin_v0_1_CURLOPT_HTTPHEADER() {
+  return CURLOPT_HTTPHEADER;
+}
+
+__attribute__((always_inline))
+int neut_rin_v0_1_CURLOPT_POSTFIELDS() {
+  return CURLOPT_POSTFIELDS;
+}
+
+__attribute__((always_inline))
+int neut_rin_v0_1_CURLOPT_POSTFIELDSIZE() {
+  return CURLOPT_POSTFIELDSIZE;
+}
+
+__attribute__((always_inline))
+int neut_rin_v0_1_CURLOPT_URL() {
+  return CURLOPT_URL;
+}
+
+__attribute__((always_inline))
+int neut_rin_v0_1_CURLOPT_WRITEFUNCTION() {
+  return CURLOPT_WRITEFUNCTION;
+}
+
+__attribute__((always_inline))
+int neut_rin_v0_1_CURLOPT_WRITEDATA() {
+  return CURLOPT_WRITEDATA;
+}
+
+__attribute__((always_inline))
+int neut_rin_v0_1_CURLOPT_HEADERFUNCTION() {
+  return CURLOPT_HEADERFUNCTION;
+}
+
+__attribute__((always_inline))
+int neut_rin_v0_1_CURLOPT_HEADERDATA() {
+  return CURLOPT_HEADERDATA;
+}
+
+__attribute__((always_inline))
+int neut_rin_v0_1_CURLE_OK() {
+  return CURLE_OK;
+}
